67250176ca36b81f74cd8164909d1fe1747730ff
Santiago Castro Herrera <159493032+Luismi0415@users.noreply.github.com>
2024-04-02 10:03:36 -0500

    momento 2

---
+++ src/main/java/vehiculos/Fabricante.java
@@ -0,0 +1,77 @@
+package vehiculos;
+
+import java.util.ArrayList;
+
+public class Fabricante {
+	private String nombre;
+	private Pais pais;
+	private static int mayor = 0;
+	private static int ocurrencia;
+	private static Fabricante fabricaMayorVenta;
+	private static ArrayList<Fabricante> fabricantes=new ArrayList<>();
+	public Fabricante(String nombre, Pais pais) {
+		this.nombre = nombre;
+		this.pais = pais;	
+	}
+
+	public static Fabricante fabricaMayorVentas() {
+		for (int i=0;i<fabricantes.size();i++) {
+			ocurrencia=0;
+			for (int j=0;j<fabricantes.size();j++) {
+				if(fabricantes.get(i).getNombre().equals(fabricantes.get(j).getNombre())) {
+					ocurrencia=ocurrencia+1;
+				}
+			}
+			if(ocurrencia>=mayor) {
+				mayor=ocurrencia;
+				fabricaMayorVenta=fabricantes.get(i);
+			}
+		}
+		return fabricaMayorVenta;
+	}
+	
+	public static void agregarFabricante(Fabricante fabricante) {
+		fabricantes.add(fabricante);
+		Pais.agregarPais(fabricante.getPais());
+		}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Pais getPais() {
+		return pais;
+	}
+
+	public void setPais(Pais pais) {
+		this.pais =pais;
+	}
+
+	public static ArrayList<Fabricante> getFabricantes() {
+		return fabricantes;
+	}
+
+	public static void setFabricantes(ArrayList<Fabricante> fabricantes) {
+		Fabricante.fabricantes = fabricantes;
+	}
+
+	public int getMayor() {
+		return mayor;
+	}
+
+	public int getOcurrencia() {
+		return ocurrencia;
+	}
+
+	public Fabricante getFabricaMayorVenta() {
+		return fabricaMayorVenta;
+	}
+
+	
+	
+
+}
---
+++ src/main/java/vehiculos/Pais.java
@@ -0,0 +1,78 @@
+package vehiculos;
+
+import java.util.ArrayList;
+
+public class Pais {
+	private String nombre;
+	private static int mayor = 0;
+	private static int ocurrencia;
+	private static Pais paisMasVendedor;
+	private static ArrayList<Pais> paises=new ArrayList<>();
+
+	public Pais(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public static Pais paisMasVendedor() {
+		for (int i=0;i<paises.size();i++) {
+			ocurrencia=0;
+			for (int j=0;j<paises.size();j++) {
+				if(paises.get(i).getNombre().equals(paises.get(j).getNombre())) {
+					ocurrencia=ocurrencia+1;
+				}
+			}
+			if(ocurrencia>=mayor) {
+				mayor=ocurrencia;
+				paisMasVendedor=paises.get(i);
+			}
+		}
+		return paisMasVendedor;
+	}
+	
+	public static void agregarPais(Pais pais) {
+		paises.add(pais);
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public static int getMayor() {
+		return mayor;
+	}
+
+	public static void setMayor(int mayor) {
+		Pais.mayor = mayor;
+	}
+
+	public static int getOcurrencia() {
+		return ocurrencia;
+	}
+
+	public static void setOcurrencia(int ocurrencia) {
+		Pais.ocurrencia = ocurrencia;
+	}
+
+	public static Pais getPaisMasVendedor() {
+		return paisMasVendedor;
+	}
+
+	public static void setPaisMasVendedor(Pais paisMasVendedor) {
+		Pais.paisMasVendedor = paisMasVendedor;
+	}
+
+	public static ArrayList<Pais> getPaises() {
+		return paises;
+	}
+
+	public static void setPaises(ArrayList<Pais> paises) {
+		Pais.paises = paises;
+	}
+	
+	
+
+}
